11ec6275c93940e57abe2848304c35a32a53cb88
David <ddnewel@gmail.com>
2026-02-01 15:03:12 -0800

    Added controls change.

--- RGB_Run_SRP3D/Assets/Scripts/UI/ColorOverlay.cs
+++ RGB_Run_SRP3D/Assets/Scripts/UI/ColorOverlay.cs
@@ -13,6 +13,8 @@ public class ColorOverlay : MonoBehaviour
 
     [SerializeField] private float colorChangeDuration = .5f;
 
+    [SerializeField] private float scollCoolDown = 0.2f;
+
     private enum ColorIndex
     {
         Red = 0,
@@ -22,6 +24,8 @@ public class ColorOverlay : MonoBehaviour
     }
     
     [SerializeField] private ColorIndex _activeColorIndex = ColorIndex.None;
+
+    private ColorIndex _previousColorIndex = ColorIndex.None;
 
     [SerializeField] private bool canToggleOff = true;
 
@@ -33,10 +37,41 @@ public class ColorOverlay : MonoBehaviour
 
     void OnEnable()
     {
+        // EnableLegacyBindings();
+
+        EnableNewBindings();
+    }
+
+    private void EnableNewBindings()
+    {
+        var onOffAction = InputSystem.actions.FindAction("ToggleFilter");
+        var scrollUpAction = InputSystem.actions.FindAction("ScrollUpFilter");
+        var scrollDownAction = InputSystem.actions.FindAction("ScrollDownFilter");
+
+        ObservableUtils.InputActionStartedAsObservable(onOffAction)
+            .TakeUntil(_ => !this.isActiveAndEnabled)
+            .Debounce(TimeSpan.FromSeconds(scollCoolDown))
+            .Subscribe(OnColorToggle)
+            .AddTo(this);
+
+        ObservableUtils.InputActionStartedAsObservable(scrollUpAction)
+            .TakeUntil(_ => !this.isActiveAndEnabled)
+            .Debounce(TimeSpan.FromSeconds(scollCoolDown))
+            .Subscribe(OnColorScrollUp)
+            .AddTo(this);
+
+        ObservableUtils.InputActionStartedAsObservable(scrollDownAction)
+            .TakeUntil(_ => !this.isActiveAndEnabled)
+            .Debounce(TimeSpan.FromSeconds(scollCoolDown))
+            .Subscribe(OnColorScrollDown)
+            .AddTo(this);
+    }
+
+    private void EnableLegacyBindings()
+    {
         var toggleRedAction = InputSystem.actions.FindAction("ToggleRedFilter");
         var toggleGreenAction = InputSystem.actions.FindAction("ToggleGreenFilter");
         var toggleBlueAction = InputSystem.actions.FindAction("ToggleBlueFilter");
-
 
         var toggleRedObservable = MakeActionStartObservable(ColorIndex.Red, toggleRedAction);
         var toggleGreenObservable = MakeActionStartObservable(ColorIndex.Green, toggleGreenAction);
@@ -47,30 +82,86 @@ public class ColorOverlay : MonoBehaviour
 
         allColorTogglesObservable
             .TakeUntil(_ => !this.isActiveAndEnabled)
-            .Subscribe(OnColorToggle)
+            .Subscribe(OnColorRGBSelect)
             .AddTo(this);
-
-        Observable<(ColorIndex, InputAction.CallbackContext)> MakeActionStartObservable(ColorIndex colorIndex, InputAction inputAction)
-        {
-            return ObservableUtils.InputActionStartedAsObservable(inputAction)
-                .Select(context => (colorIndex, context));
-        }
     }
 
-    void OnColorToggle((ColorIndex, InputAction.CallbackContext) toggleEvent)
+    Observable<(ColorIndex, InputAction.CallbackContext)> MakeActionStartObservable(ColorIndex colorIndex, InputAction inputAction)
     {
-        //Debug.Log($"OnColorToggle: {toggleEvent}");
+        return ObservableUtils.InputActionStartedAsObservable(inputAction)
+            .Select(context => (colorIndex, context));
+    }
+
+    void OnColorRGBSelect((ColorIndex, InputAction.CallbackContext) toggleEvent)
+    {
         var (colorIndex, _) = toggleEvent;
         if (canToggleOff && colorIndex == _activeColorIndex)
         {
             // This is a toggle-off event, turn off overlay image
-            _activeColorIndex = ColorIndex.None;
+            SetColor(ColorIndex.None);
         }
         else
         {
-            _activeColorIndex = colorIndex;
+            SetColor(colorIndex);
         }
-        SetFilter(_activeColorIndex);
+    }
+
+    void OnColorScrollUp(InputAction.CallbackContext scrollEvent)
+    {
+        ColorIndex newColor;
+        switch (_activeColorIndex)
+        {
+            case ColorIndex.Red:
+                newColor = ColorIndex.Green;
+                break;
+            case ColorIndex.Green:
+                newColor = ColorIndex.Blue;
+                break;
+            case ColorIndex.Blue:
+            case ColorIndex.None:
+            default:
+                newColor = ColorIndex.Red;
+                break;
+        }
+        SetColor(newColor);
+    }
+
+    void OnColorScrollDown(InputAction.CallbackContext scrollEvent)
+    {
+        ColorIndex newColor;
+        switch (_activeColorIndex)
+        {
+            case ColorIndex.Red:
+            case ColorIndex.None:
+                newColor = ColorIndex.Blue;
+                break;
+            case ColorIndex.Green:
+                newColor = ColorIndex.Red;
+                break;
+            case ColorIndex.Blue:
+            default:
+                newColor = ColorIndex.Green;
+                break;
+        }
+        SetColor(newColor);
+    }
+
+    void OnColorToggle(InputAction.CallbackContext toggleEvent)
+    {
+        if (_activeColorIndex != ColorIndex.None)
+            SetColor(ColorIndex.None);
+        else if (_previousColorIndex != ColorIndex.None)
+            SetColor(_previousColorIndex);
+        else
+            SetColor(ColorIndex.Red);
+    }
+
+    void SetColor(ColorIndex newColor)
+    {
+        _previousColorIndex = _activeColorIndex;
+        _activeColorIndex = newColor;
+
+        SetFilter(newColor);
     }
 
     void SetFilter(ColorIndex colorIndex)
