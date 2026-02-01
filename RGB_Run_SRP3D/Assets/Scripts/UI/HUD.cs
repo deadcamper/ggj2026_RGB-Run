d5d82b955aac95c10b55ef32df8eec4730985c54
David <ddnewel@gmail.com>
2026-02-01 12:29:10 -0800

    Add support for the PlayerHUD into the filter canvas. Changed it to white to match with the color changing magic :)
    Also added GameManagers.prefab, which will need to go in the Game scene asap.

--- RGB_Run_SRP3D/Assets/Scripts/UI/HUD.cs
+++ RGB_Run_SRP3D/Assets/Scripts/UI/HUD.cs
@@ -9,6 +9,12 @@ public class HUD : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnEnable()
     {
+        ScoreManager scoreMgr = Services.instance.Get<ScoreManager>();
+        if (! scoreMgr)
+        {
+            Debug.LogError("Failed to find the ScoreManager in Services. HUD will have default texts.", this);
+            return;
+        }
         Services.instance.Get<ScoreManager>().Score
             .TakeUntil(_ => !this.isActiveAndEnabled)
             .SubscribeToText(_scoreLabel, score=> $"Score: {score}")
