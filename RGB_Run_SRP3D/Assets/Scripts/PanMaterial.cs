ef08d3e08a1885ae3e554abaef415e0114bdaaf2
Jim Niehues <3d.smith@gmail.com>
2026-02-01 16:02:47 -0800

    Wireframe Player

--- RGB_Run_SRP3D/Assets/Scripts/PanMaterial.cs
+++ RGB_Run_SRP3D/Assets/Scripts/PanMaterial.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Renderer))]
 public class PanMaterial : MonoBehaviour
 {
     public float speed = .01f;
-    MeshRenderer MeshRenderer => _meshRenderer ?? (_meshRenderer = GetComponent<MeshRenderer>());
-    MeshRenderer _meshRenderer;
+    Renderer Renderer => _renderer ?? (_renderer = GetComponent<Renderer>());
+    Renderer _renderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        MeshRenderer.material.mainTextureOffset = new Vector2(Random.value, Random.value) * 10;
+        Renderer.material.mainTextureOffset = new Vector2(Random.value, Random.value) * 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MeshRenderer.material.mainTextureOffset += new Vector2(Time.deltaTime, Time.deltaTime) * speed;
+        Renderer.material.mainTextureOffset += new Vector2(Time.deltaTime, Time.deltaTime) * speed;
     }
 }
